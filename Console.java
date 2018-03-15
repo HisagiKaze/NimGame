55b38e83006885443711d1ded7a71aa021019f58
POINOT Paul-Aurian <poinot.p@gmail.com>
2018-03-15 09:12:18 +0100

    Console.java : Creation

---
+++ Console.java
@@ -0,0 +1,17 @@
+class Console
+{
+	public static int getSize()
+	{}
+
+	public static getMove(String name)
+	{}
+
+	public static ShowBoard(int[] Board)
+	{}
+
+	public static void invalidMove(Move move)
+	{}
+
+	public static void showWinner(String name)
+	{}
+}
