6c25a2606b784c04df06d650b0a1facc39ec88ee
N'GONDO Cédric <cedric.ngondo@gmail.com>
2018-03-17 05:39:45 +0100

    class IA_smart update

--- IA_smart.java
+++ IA_smart.java
@@ -1,4 +1,41 @@
 class IA_smart
 {
+    String name;
+    int nbWin;
+
+    public IA_smart()
+    {
+	this.name = "Bot_smart";
+	this.nbWin++;
+    }
+
+    public String getname()
+    {
+	return (this.name);
+    }
+    public int getnbWin()
+    {
+	return (this.nbWin);
+    }
+
+    public void setnbWin(int w)
+    {
+	this.nbWin = w;
+    }
+
+
+    public int
+
+    public static int choiceLine()
+    {
+	int choice;
+
+	choice = 0;
+	
+    }
+
+   
+
+    
     
 }
