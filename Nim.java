0045b46dc2263bf6f5e597e7b3ca88f3247f89b4
POINOT Paul-Aurian <poinot.p@gmail.com>
2018-03-29 16:56:54 +0200

    add main_IA (correction)

--- Nim.java
+++ Nim.java
@@ -5,42 +5,105 @@
  */
 class Nim
 {
-	public static void main(String[] args) 
+	public static void main_IA()
 	{
-		GameState 		currentGame;
-		HumanPlayer		currentPlayer;
-		boolean			bWantToPlayAgain;
-		Board			table;
-		int				i;
+		GameState 	currentGame;
+		HumanPlayer	player;
+		IA_naive 	artInte; 		// Artificial Inteligence
+		boolean		bWantToPlayAgain;
+		boolean		bIAWin;
+		Board 		table;
+		int			i;
 
-		Console.script(0);
-		currentPlayer = Console.getName();
-		Console.script(1);
+		Console.script(0); 			// Greetings & getName
+		player = Console.getName(1);
+		artInte = new IA_naive();
+		Console.script(1); 			// table size
 		bWantToPlayAgain = true;
+		bIAWin = false;
 		while (bWantToPlayAgain)
 		{
 			currentGame = new GameState();
 			table = new Board();
 			i = 0;
-			Console.clear_term();
 			while (table.getNbMatchLeft() > 1)
 			{
 				Console.ShowBoard(table.getBoard());
-				if (table.setBoard(Console.getMove(currentPlayer.getName())))
+				if (table.setBoard(Console.getMove(player.getName())))
 				{
 					currentGame.setNbMove();
-					currentPlayer = currentPlayer.getNext();
+					if (table.getNbMatchLeft() > 1)
+					{
+						bIAWin = ArtInte.iaPlays(table.getBoard());
+						currentGame.setNbMove();
+					}
+					Console.clear_term();
 				}
-				Console.clear_term();
 				Console.printNbMatchLeft(table.getNbMatchLeft());
+				if (bIAWin)
+					break;
 			}
-			if (table.getNbMatchLeft() < 1)
-				Console.showWinner(currentPlayer.getName());
+			if (bIAWin)
+			{
+				Console.script(3); 	// IA a gagnée
+				artInte.setNbWins();
+			}
 			else
-				Console.showWinner(currentPlayer.getNext().getName());
+			{
+				Console.showWinner(player);
+				player.setNbWins();
+			}
 			Console.nbMovePlayed(currentGame.getNbMove());
 			currentGame.setState(false);
 			bWantToPlayAgain = Console.askToPlayAgain();
+		}
+	}
+
+	/**
+	 * Appelle toutes les fonctions filles et gère le bon déroulement du jeu.
+	 * @param args Arguments passés en parametre lors de l'appelle de la fonction.
+	 */
+	public static void main(String[] args) 
+	{
+		if (Console.askAnIA())
+			main_IA();
+		else
+		{
+			GameState 		currentGame;
+			HumanPlayer		currentPlayer;
+			boolean			bWantToPlayAgain;
+			Board			table;
+			int				i;
+
+			Console.script(0);
+			currentPlayer = Console.getName(2);
+			Console.script(1);
+			bWantToPlayAgain = true;
+			while (bWantToPlayAgain)
+			{
+				currentGame = new GameState();
+				table = new Board();
+				i = 0;
+				while (table.getNbMatchLeft() > 1)
+				{
+					Console.ShowBoard(table.getBoard());
+					if (table.setBoard(Console.getMove(currentPlayer.getName())))
+					{
+						currentGame.setNbMove();
+						currentPlayer = currentPlayer.getNext();
+						Console.clear_term();
+					}
+					Console.printNbMatchLeft(table.getNbMatchLeft());
+				}
+				if (table.getNbMatchLeft() < 1)
+					Console.showWinner(currentPlayer.getName());
+				else
+					Console.showWinner(currentPlayer.getNext().getName());
+				currentPlayer.setNbWins();
+				Console.nbMovePlayed(currentGame.getNbMove());
+				currentGame.setState(false);
+				bWantToPlayAgain = Console.askToPlayAgain();
+			}
 		}
 	}
 }
